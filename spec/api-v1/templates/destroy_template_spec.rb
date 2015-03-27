a4cdacb386d9740f7a440418baec8555956f2a2f
Przemysław Ciąćka <przemyslaw.ciacka@gmail.com>
2015-03-28 00:13:29 +0100

    Optimized specs for DELETE /v1/templates/:id

--- spec/api-v1/templates/destroy_template_spec.rb
+++ spec/api-v1/templates/destroy_template_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 describe "DELETE /api/v1/templates/:id", type: :request do
   let(:template) { create(:template) }
+  let(:delete_url) { api_v1_template_url(template) }
 
   before :each do
-    delete api_v1_template_url(template)
+    delete delete_url
   end
 
   it { expect(response).to have_http_status(200) }
@@ -14,6 +15,14 @@ describe "DELETE /api/v1/templates/:id", type: :request do
   context "when trying to get removed template" do
     before :each do
       get api_v1_template_url(template)
+    end
+
+    it_should_return "resource_not_found error"
+  end
+
+  context "when trying to remove unexisting template" do
+    before :each do
+      delete delete_url
     end
 
     it_should_return "resource_not_found error"
